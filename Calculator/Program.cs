6e74893faa807e26039bf2f16d0044e71452257d
AHdev22 <amr.h.dev22@gmail.com>
2026-08-15 06:08:30 +0300

    Updated Calculator logic

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,2 +1,51 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello!");
+Console.WriteLine("Input the first number: ");
+int firstInput = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Input the Second number: ");
+int secondInput = int.Parse(Console.ReadLine()!);
+Console.WriteLine("What do you want to do?");
+Console.WriteLine("[A]dd numbers");
+Console.WriteLine("[S]ubtract numbers");
+Console.WriteLine("[M]ultiply numbers");
+string userChoice = Console.ReadLine()!;
+
+if (userChoice == "a" || userChoice == "A")
+{
+    Console.WriteLine(firstInput + " + " + secondInput + " = " + AddInputs(firstInput, secondInput));
+    CloseMessage();
+}
+else if (userChoice == "s" || userChoice == "S")
+{
+    Console.WriteLine(firstInput + " - " + secondInput + " = " + SubtractInputs(firstInput, secondInput));
+    CloseMessage();
+}
+else if (userChoice == "m" || userChoice == "M")
+{
+    Console.WriteLine(firstInput + " * " + secondInput + " = " + MultiplyInputs(firstInput, secondInput));
+    CloseMessage();
+}
+else
+{
+    Console.WriteLine("Invalid Choice!");
+    CloseMessage();
+}
+
+int AddInputs(int num1 , int num2)
+{
+    return num1 + num2;
+}
+int SubtractInputs(int num1 , int num2)
+{
+    return num1 - num2;
+}
+int MultiplyInputs(int num1 , int num2)
+{
+    return num1 * num2;
+}
+
+void CloseMessage()
+{
+    Console.WriteLine("Press any key to close");
+}
+
 Console.ReadKey();
